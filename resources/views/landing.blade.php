fd8b0175b90e0732e1d682e63aa9abf22061212b
@RubenSianipar <iss22026@students.del.ac.id>
2025-03-25 08:05:30 +0700

    1111

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -28,34 +28,38 @@
 <body>
 
     <!-- Navigation -->
+     
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="">Capcapung</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item mr-sm-3 mb-2 mb-sm-0">
-                        <div class="dropdown">
-                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                @lang('landing.langtext')
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="{{url('id')}}">Indonesia</a>
-                                <a class="dropdown-item" href="{{url('en')}}">Inggris</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-success" href="{{url('login')}}">@lang('landing.loginOrRegister')</a>
-                    </li>
-                </ul>
+    <div class="container d-flex justify-content-between align-items-center">
+        <!-- Capcapung ke kiri -->
+        <a class="navbar-brand" href="">Capcapung</a>
+
+      <!-- Search di tengah -->
+    <form class="d-flex flex-grow-1 justify-content-center" style="max-width: 600px;">
+        <input class="form-control me-2" type="search" placeholder="Cari artikel..." 
+            aria-label="Search" style="width: 100%; height: 40px; font-size: 16px;">
+        <button class="btn btn-outline-light" type="submit" style="height: 39px;">Cari</button>
+    </form>
+
+
+        <!-- Bahasa & Login ke kanan -->
+        <div class="d-flex">
+            <div class="dropdown me-2">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    @lang('landing.langtext')
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="{{url('id')}}">Indonesia</a>
+                    <a class="dropdown-item" href="{{url('en')}}">Inggris</a>
+                </div>
             </div>
+            <a class="btn btn-success" href="{{url('login')}}">@lang('landing.loginOrRegister')</a>
         </div>
-    </nav>
+    </div>
+</nav>
+
+
 
   <!-- Header -->
 <!-- Header -->
@@ -83,7 +87,7 @@
 
 
     <!-- <section class="text-center p-5"> -->
-    <section class="p-5 text-center" style="background: white;">
+    <section class="p-5 text-center" style="background: #343a40; color: white;">
         <h3>@lang('landing.why')</h3>
     </section>
 
@@ -93,7 +97,7 @@
         <div class="row">
             <div class="col-lg-6">
                 <h4>{{ __('landing.title1') }}</h4>
-                <p>{{ __('landing.content1') }}</p>
+                <p class="text-justify">{{ __('landing.content1') }}</p>
             </div>
             <div class="col-lg-6 d-none d-lg-block text-center">
                 <div class="embed-responsive embed-responsive-16by9">
@@ -105,6 +109,7 @@
         </div>
     </div>
 </section>
+
 
 
 
@@ -123,7 +128,7 @@
             <!-- Teks di Kanan -->
             <div class="col-lg-6">
                 <h4>{{ __('landing.title2') }}</h4>
-                <p>{{ __('landing.content2') }}</p>
+                <p class="text-justify">{{ __('landing.content2') }}</p>
             </div>
         </div>
     </div>
@@ -138,7 +143,7 @@
             <!-- Teks di Kiri -->
             <div class="col-lg-6">
                 <h4>{{ __('landing.title3') }}</h4>
-                <p>{{ __('landing.content3') }}</p>
+                <p class="text-justify">{{ __('landing.content3') }}</p>
             </div>
             <!-- Video di Kanan -->
             <div class="col-lg-6 d-none d-lg-block text-center">
@@ -165,7 +170,7 @@
                 <div class="card">
                     <iframe src="{{ asset('art/capcapung.pdf') }}" class="card-img-top" style="width: 100%; height: 300px;" frameborder="0"></iframe>
                     <div class="card-body d-none d-lg-block">
-                        <p class="card-text">Artikel 1</p>
+                        <p class="card-text">Artikel Pertanian</p>
                     </div>
                 </div>
             </div>
@@ -173,7 +178,7 @@
                     <div class="card">
                         <img src="{{asset('img/landing/Celana.jpg')}}" class="card-img-top" alt="">
                         <div class="card-body d-none d-lg-block">
-                            <p class="card-text">Artikel 2</p>
+                            <p class="card-text">Artikel Peternakan</p>
                         </div>
                     </div>
                 </div>
@@ -181,15 +186,7 @@
                     <div class="card">
                         <img src="{{asset('img/landing/Jas.jpg')}}" class="card-img-top" alt="">
                         <div class="card-body d-none d-lg-block">
-                            <p class="card-text">Artikel 3</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 mb-2">
-                    <div class="card">
-                        <img src="{{asset('img/landing/Selimut.jpg')}}" class="card-img-top" alt="">
-                        <div class="card-body d-none d-lg-block">
-                            <p class="card-text">Artikel 4</p>
+                            <p class="card-text">Artikel Lainnya</p>
                         </div>
                     </div>
                 </div>
@@ -198,12 +195,12 @@
     </section>
 
     <section class="p-5 text-center" style="background: white;">
-     <!-- <section class="text-center p-5"> -->
-    <button class="btn btn-primary">Tambahkan Artikel</button>
+    <a href="/login" class="btn btn-primary">Tambahkan Artikel</a>
 </section>
 
+
  <!-- <section class="text-center bg-blue" > -->
-    <section class="p-5 text-center" style="background: white;">
+    <section class="p-5 text-center" style="background: #343a40; color: white;">
         <h3>{{ __('landing.temukan') }}</h3>
     </section>
 
@@ -213,7 +210,8 @@
         <div class="row">
             <div class="col-md-6 mb-4 mb-sm-0">
                 <h5>Alamat</h5>
-                <p>Jalan Sianipar Balige</p>
+                <p>Jl. Sisingamangaraja, Sitoluama Laguboti, Toba Samosir Sumatera Utara, Indonesia </p>
+                <p>Kode Pos: 22381</p>
                 <br>
                 <h5>Kunjungi akun resmi Instagram kami</h5>
                 <p>
@@ -236,11 +234,13 @@
             </div>
             <div class="col-md-6">
                 <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63784.65951407885!2d99.0681676!3d2.3223929!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e04584fee0d55%3A0xd13973a06cb33d47!2sJl.%20Sianipar%2C%20Pardede%20Onan%2C%20Kec.%20Balige%2C%20Toba%2C%20Sumatera%20Utara!5e0!3m2!1sen!2sid!4v1734233039352!5m2!1sen!2sid"
-                        width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""
-                        aria-hidden="false" tabindex="0"></iframe>
-                </div>
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3986.3673457400764!2d99.14863280000002!3d2.3832152000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e00fdad2d7341%3A0xf59ef99c591fe451!2sDel%20Institute%20of%20Technology!5e0!3m2!1sen!2sid!4v1742264473174!5m2!1sen!2sid"
+                    width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
             </div>
+
+
         </div>
     </section>
     <!-- /.container -->
